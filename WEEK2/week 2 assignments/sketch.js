fbcccc09915116181d19b5fd8c8d9b91d42963ce
aledugarte <alexandradugarte@Alexandras-MacBook-Pro-2.local>
2017-02-01 20:27:09 -0500

    assignment

--- WEEK2/week 2 assignments/sketch.js
+++ WEEK2/week 2 assignments/sketch.js
@@ -1,7 +1,20 @@
+var 
+
+
 function setup() {
 
+    createCanvas(500,600);
+    background(0,0,0);
+    
 }
 
 function draw() {
-  
+
+    rect (220,340,75,35); //neck
+    rect (150,375,250,200); //shirt
+    ellipse (245,200,260,300); //head
+    ellipse (200,250,110,60); //eye1 
+    ellipse (150,150,23,30); //eye2
+    ellipse (250,150,25,32);
+    
 }
--- WEEK2/week 2 notes/sketch.js
+++ WEEK2/week 2 notes/sketch.js
@@ -1,7 +1,71 @@
+var num = 100;
+var num2 = 50;
+var message = "hello";
+var message2 = "goodbye";
+var booleanVar = true;
+var booleanVar2 = false;
+var mouthX = 25;
+var mouthY = 250;
+var mouthW = 400;
+var mouthH = 60;
+
 function setup() {
+    console.log(num);
+    console.log("num + num2" + (num + num2));
+    console.log(num * num2);
+    console.log(num / num2);   
+    console.log(message + " " + message2);    
+    console.log(booleanVar);
+
+    console.log(message + num2);
+
+
+        //this is not going to work
+    console.log(message * num2);  
+        // this will also not work
+        //console.log(message/num2;)
+
+        /*
+        If you want to write more stuff, 
+        use a multi-line
+        comment
+
+        */
+
+    //var message2 = "This only works in the setup";
+    //console.log(message2); 
+    //console.log(message3); ERROR   
+
+
+    createCanvas(500,400);
+    background(0,0,0);
+    background(255,0,0);
+    background(125);
+    
+    
+    console.log("width: " + width + " " + "height: " + height)
+    //this is to see in the console the current width and height
+    
+    
 
 }
 
 function draw() {
-  
+    //  console.log(num); 
+    //  console.log(message2); ERROR
+    //  var message3 = "this only works in the draw" ERROR
+    
+    
+    //(x,y,w,h)
+    ellipse(70,70,100,100);
+    ellipse(width/2,height/2,50,50);
+    //put it like this so its always centered even if you change canvas size.
+    
+    rect(mouthX, mouthY, mouthW, mouthH); 
+    line (mouthX, mouthY + mouthH/2, mouthX + mouthW, mouthY + mouthH/2);
+    
+    
+    
+    
+    
 }
