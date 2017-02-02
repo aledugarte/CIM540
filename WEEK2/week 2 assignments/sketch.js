a7eba141035ef945b32c87bfb50156d38e10b9f9
aledugarte <alexandradugarte@Alexandras-MacBook-Pro-2.local>
2017-02-02 09:26:44 -0500

    ASSIGNMENT

--- WEEK2/week 2 assignments/sketch.js
+++ WEEK2/week 2 assignments/sketch.js
@@ -1,4 +1,8 @@
-var 
+var HeadX = 250;
+var HeadY = 200;
+var HeadW = 260;
+var headH = 300;
+var mouthX = 200;
 
 
 function setup() {
@@ -10,11 +14,11 @@ function setup() {
 
 function draw() {
 
-    rect (220,340,75,35); //neck
-    rect (150,375,250,200); //shirt
-    ellipse (245,200,260,300); //head
-    ellipse (200,250,110,60); //eye1 
-    ellipse (150,150,23,30); //eye2
-    ellipse (250,150,25,32);
+    rect (214,340,75,35); //neck
+    rect (HeadX/2,375,HeadW,200); //shirt
+    ellipse (HeadX,HeadY,HeadW,headH); //head
+    ellipse (mouthX,250,110,60); //mouth
+    ellipse (HeadX-100,150,23,30); //eye1
+    ellipse (HeadX,150,25,32); //eye2
     
 }
